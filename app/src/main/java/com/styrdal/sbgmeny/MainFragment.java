9f9bad9fd085e6b1e57cbf43cc7bcabd49d0b71f
Haak0n <styrdal@gmail.com>
2015-08-10 02:35:25 +0200

    Minor dependency cleanup

--- app/src/main/java/com/styrdal/sbgmeny/MainFragment.java
+++ app/src/main/java/com/styrdal/sbgmeny/MainFragment.java
@@ -11,7 +11,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,7 +18,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
 public class MainFragment extends ListFragment 
